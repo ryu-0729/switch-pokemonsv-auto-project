2bb2a4b52db77a86653c1db26af1cf82815dff52
ryu-0729 <1844ryudai@gmail.com>
2023-01-05 23:31:47 +0900

    feat: Adding Custom class

---
+++ CustomCommon.py
@@ -0,0 +1,76 @@
+import time
+import cv2 as cv
+import numpy as np
+from nxbt import Buttons
+from nxbt import Sticks
+
+
+class CustomCommon():
+    def __init__(self, nx, cIndex):
+        self.nx = nx
+        self.cIndex = cIndex
+
+
+    def game_start(self):
+        """ NOTE: コントローラーの持ち方/順番を変える画面からゲームを起動するまで """
+
+        for _ in range(2):
+            self.nx.press_buttons(self.cIndex, [Buttons.B], down=1.0)
+            time.sleep(2)
+
+        for _ in range(4):
+            self.nx.tilt_stick(self.cIndex, Sticks.LEFT_STICK, -100, 0, 0.25, 0.25)
+
+        self.nx.tilt_stick(self.cIndex, Sticks.LEFT_STICK, 0, 100, 0.25, 0.25)
+        for _ in range(2):
+            self.nx.press_buttons(self.cIndex, [Buttons.A], down=1.0)
+
+
+    def get_capture(self):
+        """ NOTE: キャプチャの取得と画像の返却 """
+
+        cap = cv.VideoCapture(0)
+
+        for _ in range(60):
+            ret, frame = cap.read()
+
+        return frame
+
+
+    def check_report(self, cap_img):
+        """ NOTE: キャプチャ画像と学校最強大会エントリー画面の「学校最強大会」の画像と一致するか判定 """
+
+        # NOTE: キャプチャ画像のトリミングと2値化処理
+        trimming_cap_img = cap_img[125 : 175, 400 : 525]
+        gray_cap_img = cv.cvtColor(trimming_cap_img, cv.COLOR_RGB2GRAY)
+        _, thresh_cap_img = cv.threshold(gray_cap_img, 0, 255, cv.THRESH_OTSU)
+
+        # NOTE: エントリー画像の読み込みと2値化処理
+        img = cv.imread('./img/trimming.jpg')
+        gray_img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+        _, thresh_img = cv.threshold(gray_img, 0, 255, cv.THRESH_OTSU)
+
+        # NOTE: 一致する要素数を取得し、既定値を超えているか判定
+        ret = np.count_nonzero(thresh_cap_img == thresh_img)
+        print('画像比較中')
+        print(ret)
+        return ret >= 5500
+
+
+    def report(self):
+        """ NOTE: レポートを書く """
+
+        self.nx.press_buttons(self.cIndex, [Buttons.X], down=1.0)
+        self.nx.press_buttons(self.cIndex, [Buttons.R], down=1.0)
+        time.sleep(5)
+        self.nx.press_buttons(self.cIndex, [Buttons.A], down=1.0)
+        time.sleep(10)
+        self.nx.press_buttons(self.cIndex, [Buttons.B], down=1.0)
+        self.nx.press_buttons(self.cIndex, [Buttons.B], down=1.0)
+
+
+    def streak_button(self):
+        """ NOTE: RボタンとAボタンの連打 """
+
+        self.nx.press_buttons(self.cIndex, [Buttons.R])
+        self.nx.press_buttons(self.cIndex, [Buttons.A])
--- makingMoneyV2.py
+++ makingMoneyV2.py
@@ -1,11 +1,8 @@
-import time
 from random import randint
 import datetime
 import nxbt
-from nxbt import Buttons
-from nxbt import Sticks
-import cv2
-import numpy as np
+
+from CustomCommon import CustomCommon
 
 
 def random_colour():
@@ -15,54 +12,6 @@ def random_colour():
         randint(0, 255),
         randint(0, 255),
     ]
-
-def start_game(nx, cIndex):
-    for _ in range(2):
-        nx.press_buttons(cIndex, [Buttons.B], down=1.0)
-        time.sleep(2)
-    
-    for _ in range(4):
-        nx.tilt_stick(cIndex, Sticks.LEFT_STICK, -100, 0, 0.25, 0.25)
-
-    nx.tilt_stick(cIndex, Sticks.LEFT_STICK, 0, 100, 0.25, 0.25)
-    for _ in range(2):
-        nx.press_buttons(cIndex, [Buttons.A], down=1.0)
-
-def capture():
-    cap = cv2.VideoCapture(0)
-
-    for _ in range(60):
-        ret, frame = cap.read()
-
-    # NOTE: キャプチャ画面を返却
-    return frame
-
-# TODO: レポートするかの判定は画像比較を行う
-def check_report():
-    cap_img = capture()
-    # NOTE: キャプチャ画像のトリミングと2値化処理
-    trimming_cap_img = cap_img[125 : 175, 400 : 525]
-    gray_cap_img = cv2.cvtColor(trimming_cap_img, cv2.COLOR_RGB2GRAY)
-    _, thresh_cap_img = cv2.threshold(gray_cap_img, 0, 255, cv2.THRESH_OTSU)
-
-    # NOTE: エントリー画像の読み込みと2値化処理
-    img = cv2.imread('./img/trimming.jpg')
-    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    _, thresh_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_OTSU)
-
-    ret = np.count_nonzero(thresh_cap_img == thresh_img)
-    print('画像比較')
-    print(ret)
-    return ret >= 5500
-
-def report(nx, cIndex):
-    nx.press_buttons(cIndex, [Buttons.X], down=1.0)
-    nx.press_buttons(cIndex, [Buttons.R], down=1.0)
-    time.sleep(5)
-    nx.press_buttons(cIndex, [Buttons.A], down=1.0)
-    time.sleep(10)
-    nx.press_buttons(cIndex, [Buttons.B], down=1.0)
-    nx.press_buttons(cIndex, [Buttons.B], down=1.0)
 
 def main():
     # Init NXBT
@@ -89,10 +38,12 @@ def main():
 
     # Wait for the switch to connect to the controller
     nx.wait_for_connection(controller_index)
+    print('接続完了！')
+
+    custom = CustomCommon(nx, controller_index)
 
     # NOTE: ゲームの起動
-    start_game(nx, controller_index)
-    time.sleep(25)
+    custom.game_start()
 
     now = datetime.datetime.now()
     # NOTE: 任意の時間を設定
@@ -100,14 +51,14 @@ def main():
 
     # TODO: 設定金額を設けて金額に達したら処理を終了したい
     while True:
-        if check_report():
+        cap_img = custom.get_capture()
+        if custom.check_report(cap_img):
             # NOTE: レポート実行
-            report(nx, controller_index)
+            custom.report()
             # NOTE: 設定時間を経過しているかつレポートが終わったタイミングで処理終了
             if not datetime.datetime.now() < now + after_hour:
                 break
-        nx.press_buttons(controller_index, [Buttons.R])
-        nx.press_buttons(controller_index, [Buttons.A])
+        custom.streak_button()
 
     print('処理終了...')
 
